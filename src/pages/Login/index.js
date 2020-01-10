3cc285daed6b429e6c9e464b6c6898f5a07a9028
inajarafp8 <inajarafp8@gmail.com>
2020-01-09 22:06:27 -0300

    Vitrine Frontend

---
+++ src/pages/Login/index.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import api from '../../services/api';
+
+export default function Login({history}){
+
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event){
+      event.preventDefault();
+  
+      const response= await api.post('/sessions', {email});
+  
+      const {_id} = response.data;
+
+      localStorage.setItem('user', _id);
+
+      history.push('/dashboard');
+    }
+
+    return (
+
+        <>
+            <p>Ofereça os melhores <strong> produtos </strong> com preços promocionais na sua <strong>cidade </strong> 
+            e <strong>região</strong>
+            </p>
+
+            <form onSubmit= {handleSubmit}>
+            <label htmlFor="email">E-mail</label>
+            <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)} />
+
+            <button type="submit" className="btn">Entrar</button>
+            </form>
+        </>
+    )
+}
